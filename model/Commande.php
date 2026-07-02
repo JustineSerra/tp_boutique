a85ad939c38fb4cfeadf6b359459b800f6992266
Justine Serra <bridel44@hotmail.fr>
2026-07-02 11:01:25 +0200

    model commande en cours

--- model/Commande.php
+++ model/Commande.php
@@ -1,1 +1,24 @@
 <?php
+
+class Commande {
+    private ?int $id;
+    private int $userId;
+    private string $createdAt;
+    private int $total;
+
+    public function __construct(?int $id, int $userId, string $createdAt, int $total) {
+        $this->id = $id;
+        $this->userId = $userId;
+        $this->createdAt = $createdAt;
+        $this->total = $total;
+    }
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+    public function setId(?int $id): self {
+        $this->id = $id;
+        return $this;
+    }
+
+}
