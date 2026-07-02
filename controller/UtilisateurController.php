10135de147fdeeaa9200f860c4ffe0348da5e458
Justine Serra <bridel44@hotmail.fr>
2026-07-02 10:06:41 +0200

    corrections

--- controller/UtilisateurController.php
+++ controller/UtilisateurController.php
@@ -1,34 +1,38 @@
 <?php
-require_once "../config/Database.php";
 
-if (isset($_SESSION["user_id"])) {
-    // header("Location: ./view/index.php");
-    exit;
-};
+require_once __DIR__ . "/../model/Utilisateur.php";
+require_once __DIR__ . "/../repository/UtilisateurRepository.php";
 
-//verif : si le formulaire a la methode post - > je continue !!
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+// if (isset($_SESSION["user_id"])) {
+//     // header("Location: ./view/index.php");
+//     exit;
+// };
 
-    if ($email !== "" && $password !== "") {
-        $pdo = Database::getConnexion();
-        $request = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-        $request->execute(["email" => $email]);
-        $user = $request->fetch();
-        var_dump($user);
-        $passwordVerif = password_verify($password, $user["password_hash"]);
+// //verif : si le formulaire a la methode post - > je continue !!
 
-        if ($passwordVerif) {
-            $_SESSION["user_id"] = $user["id"];
-            $_SESSION["user_email"] = $user["email"];
-            header("Location: /view/accueil.php");
-            exit;
-        } else {
-            $error_msg = "MOT DE PASSE OU EMAIL INCORRECT";
-            echo $error_msg;
-        }
-    }
-}
+// if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+//     $email = $_POST["email"];
+//     $password = $_POST["password"];
+
+//     if ($email !== "" && $password !== "") {
+//         $pdo = Database::getConnexion();
+//         $request = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+//         $request->execute(["email" => $email]);
+//         $user = $request->fetch();
+//         var_dump($user);
+//         $passwordVerif = password_verify($password, $user["password_hash"]);
+
+//         if ($passwordVerif) {
+//             $_SESSION["user_id"] = $user["id"];
+//             $_SESSION["user_email"] = $user["email"];
+//             header("Location: /view/accueil.php");
+//             exit;
+//         } else {
+//             $error_msg = "MOT DE PASSE OU EMAIL INCORRECT";
+//             echo $error_msg;
+//         }
+//     }
+// }
--- repository/ProduitRepository.php
+++ repository/ProduitRepository.php
@@ -1,6 +1,5 @@
 <?php 
 
-require_once __DIR__ . "/../model/Produit.php";
 require_once __DIR__ . "/../config/Database.php";
 
 class ProduitRepository {
