6c2f468a656aef8cb9da5ce96f440cc0b665733d
AssiaCherfaoui <assiacherfaoui44@gmail.com>
2026-07-03 09:41:16 +0200

    ajout css et affichage de produits

--- view/accueil.php
+++ view/accueil.php
@@ -1,16 +1,11 @@
-<?php include_once __DIR__ . "/../layout/header.php";?>
+<?php include_once __DIR__ . "/layout/header.php";
+?>
 
+<!-- //LE FOR EACH DE MERDE faut pas faire attention au undefined $produit c un bug-->
 
+<?php foreach ($produits as $produit): ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Accueil</h1>
-    <p>Bienvenue sur notre site !</p>
-</body>
-</html>
+    <?= htmlspecialchars($produit->getNom()) ?>
+    <?= htmlspecialchars($produit->getPrix()) ?>
+    <?= htmlspecialchars($produit->getDescription()); ?>
+<?php endforeach; ?>
